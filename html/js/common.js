65fe3709d801d838bd34eb756bc2ec1958a7d71c
Annie Park <pjatopazz@gmail.com>
2021-04-23 17:57:15 +0900

    commit 7th

--- html/js/common.js
+++ html/js/common.js
@@ -1,44 +1,100 @@
-const art = document.querySelectorAll('.artGroup article'),
-      artG = document.querySelectorAll('.artGroup'),
-      btPrev = document.querySelector('.btnPrev'),
-      btNext = document.querySelector('.btnNext');
-      
-      let bnr= artG[3];
-      const bnrArt = bnr.children;
-      
-      btPrev.addEventListener('click',prev);
-      btNext.addEventListener('click',next);
-      
-      art.forEach((articles)=>{
-          articles.addEventListener('click',show);
+const   art = document.querySelectorAll('.artGroup article'),
+        artG = document.querySelectorAll('.artGroup'),
+        btPrev = document.querySelector('.btnPrev'),
+        btNext = document.querySelector('.btnNext'),
+        hourEle = document.querySelectorAll('.ulGroup ul li'),
+        mainBG = document.querySelector('.mnWrap')
+        date = new Date();
+
+let bnr = artG[3];
+let cnt=0;
+const bnrArt = bnr.children;
+
+    //메인 배너 
+    mainBG.addEventListener('click',fadeInOut);
+    function fadeInOut(){
+        mainBG.animate({
+            backgroundImage :[
+                url("../image/products_main.jpg"),
+                url("../image/products_main_i2.jpg")
+            ]
+        },{
+            duration: 5000,
+            fill: 'forwards'
         });
-        
-        
-        function show(){
-            art.forEach((articles)=>{
-                articles.classList.remove('back');
-            });
-            this.classList.add('back');
+    }
+
+    //배너 전처리
+    bnr.style.marginLeft = '-100%';
+    bnr.insertBefore(bnrArt[2], bnrArt[0]);
+
+    //시간세팅
+    function worldTime(tzOffset) { // 24시간제
+        let tz = date.getTime() + (date.getTimezoneOffset() * 60000) + (tzOffset * 3600000);
+        date.setTime(tz);
+       
+        let s =
+          trim(date.getHours(), 2) + ':' +
+          trim(date.getMinutes(), 2)  
+        return s;
+      }
+       
+      function trim(n, digits) {
+        let zero = '';
+        n = n.toString();
+        if (n.length < digits) {
+          for (i = 0; i < digits - n.length; i++)
+            zero += '0';
         }
-        function prev(){
-           bnr.animate({
-                marginLeft:'0%'
-            },{
-                duration:1000,
-                easing:'ease',
-                complete: [
-                    bnr.style.marginLeft = "-100%",
-                    bnr.insertBefore(bnrArt[2],bnrArt[0])
-                ]
-            });
-        }
-        
-        function next(){0
-            bnr.style.marginLeft = "-200%";
-            bnr.appendChild(bnrArt[0]);
-            bnr.style.marginLeft = "-100%";
-        }
+        return zero + n;
+      }
 
+    //이벤트 선언
+    art.forEach((articles) => {
+        articles.addEventListener('mousemove', show);
+        articles.addEventListener('mouseleave',hide);
+    });
+    btPrev.addEventListener('click', prev);
+    btNext.addEventListener('click', next);
+    //함수
+    function show() {
+        art.forEach((articles) => {
+            articles.classList.remove('back');
+        });
+        this.classList.add('back');
+    }
 
+    function hide() {
+        art.forEach((articles) => {
+            articles.classList.remove('back');
+        });
+    }
 
-
+    function prev() {
+        bnr.animate({
+            marginLeft: [
+                '-200%',
+                '-100%'
+            ]
+        }, {
+            duration: 1000,
+            easing: 'ease',
+            fill: 'forwards'
+        });
+        bnr.insertBefore(bnrArt[2], bnrArt[0]);
+ 
+    }
+    
+    function next() {
+        bnr.animate({
+            marginLeft: [
+                '-0%',
+                '-100%'
+            ]
+        }, {
+            duration: 1000,
+            easing: 'ease',
+            fill: 'forwards'
+        });
+        bnr.appendChild(bnrArt[0]);
+    }
